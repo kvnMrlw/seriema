e85e6503694fa380980795d53003a656aedcb893
kvnMrlw <kevinmarlow2010@gmail.com>
2026-09-22 20:51:23 -0300

    inicialização do wagtail

---
+++ seriema/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-6=w7c-)&=e^e5m=g33#2zg2m0fl&kr!-1_9%wgzkjyz+*7(-*b"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
